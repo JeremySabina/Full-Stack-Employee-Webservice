b9ad2caf711343d7afcd19a8f092e34b4bd622d7
Jeremy <jeremysabina@gmail.com>
2021-10-28 15:14:28 -0400

    Adding CRUD operations

---
+++ EmployeeManager/src/main/java/Employee/Webservice/EmployeeManager/service/EmployeeService.java
@@ -0,0 +1,36 @@
+package Employee.Webservice.EmployeeManager.service;
+
+import Employee.Webservice.EmployeeManager.model.Employee;
+import Employee.Webservice.EmployeeManager.repo.EmployeeRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
+public class EmployeeService {
+    private final EmployeeRepo employeeRepo;
+
+    @Autowired
+    public EmployeeService(EmployeeRepo employeeRepo) {
+        this.employeeRepo = employeeRepo;
+    }
+
+    public Employee addEmployee (Employee employee) {
+        employee.setEmployeeCode(UUID.randomUUID().toString());
+        return employeeRepo.save(employee);
+    }
+
+    public List<Employee> findAllEmployees() {
+        return employeeRepo.findAll();
+    }
+
+    public Employee update (Employee employee) {
+        return employeeRepo.save(employee);
+    }
+
+    public void deleteEmployee(Long id) {
+        employeeRepo.deleteEmployeeById(id);
+    }
+}
